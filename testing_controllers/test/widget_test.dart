aacd41b1f60690a99226b6f3ed15f1ecf9c86538
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-07-13 09:33:18 -0500

    add mock controller

--- testing_controllers/test/widget_test.dart
+++ testing_controllers/test/widget_test.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
 import 'package:testing_controllers/ui/controllers/name_controller.dart';
 import 'package:testing_controllers/ui/hello.dart';
+
+// Una versión Mock de nuestro controlador, en ella podemos controlar los métodos
+// y atributos para tener valores fijos contra lo que podamos probar
+
+class MockNameController extends GetxService
+    with Mock
+    implements NameController {
+  var _name = 'NAME'.obs;
+
+  @override
+  String get name => _name.value;
+
+  @override
+  setName(val) => _name.value = val;
+}
 
 void main() {
   setUp(() {});
   testWidgets('Simple hello test', (WidgetTester tester) async {
-    Get.put(NameController());
+    // aquí instanciamos el mock controller
+    MockNameController mockNameController = MockNameController();
+    // lo pasamos al DI de Get
+    Get.put<NameController>(mockNameController);
     await tester.pumpWidget(const MaterialApp(
         home: Scaffold(
       body: Hello(),
@@ -17,7 +36,7 @@ void main() {
 
     expect(find.text('Testing demo'), findsOneWidget);
 
-    expect(find.text('Hello N/A'), findsOneWidget);
+    expect(find.text('Hello NAME'), findsOneWidget);
 
     await tester.enterText(find.byKey(const Key('TextFieldName')), 'Juan');
 
@@ -27,7 +46,7 @@ void main() {
 
     expect(find.text('Hello Juan'), findsOneWidget);
 
-    expect(find.text('Hello N/A'), findsNothing);
+    expect(find.text('Hello NAME'), findsNothing);
 
     expect(find.text('Testing demo'), findsOneWidget);
   });
